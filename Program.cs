83723d0f92168970b10317f08bf11206f38507e8
Rakarake <rak@rakarake.xyz>
2024-03-15 18:35:56 +0100

    Better simple example

--- Program.cs
+++ Program.cs
@@ -7,31 +7,34 @@ if (args.Count() == 0) {
 }
 
 var exampleToRun = args[0];
-Console.WriteLine("CMD ARGUMENT: " + exampleToRun);
+
+
 
 if (exampleToRun == "Simple") {
-
     void SimpleSystem(Query<(Wrapper<int>, String)> q) {
         foreach (var hit in q.hits) {
             // Retrieve the parts of the query
-            var (x, y) = hit.Value;
+            var (intWrapper, str) = hit.Value;
             Console.WriteLine("Entity ID: " + hit.Key);
-            Console.WriteLine("x: " + x.item + ", y: " + y);
+            Console.WriteLine("intWrapper: " + intWrapper.item + ", str: " + str);
             // Increment each query
-            x.item += 1;
+            intWrapper.item += 1;
         }
     }
-
+    
     Engine ecs = new();
-
+    
     ecs.AddEntity(new List<Object>() {
         new Wrapper<int>() { item = 0 },
         "Hi Mark!",
     });
+    
     ecs.AddSystem(Event.Update, SimpleSystem);
     ecs.Start();
-
 }
+
+
+
 else if (exampleToRun == "Advanced") {var e = new Advanced(); e.Example();}
 else { Console.WriteLine("Please provide which example to run in command line arguments"); }
 
